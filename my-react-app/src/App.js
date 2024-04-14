3bf17f20fed4d00a39767602b0f6899bc483f84b
lrnzoclmb <lorenzo.miguel.columba@gmail.com>
2024-04-14 23:55:24 +0800

    adjustment off navbar

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,10 +2,16 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Rout from './rout'
 import 'typeface-montserrat';
+import {Helmet} from "react-helmet";
 
 const App = () => {
   return (
     <>
+    <Helmet>
+                <meta charSet="utf-8" />
+                <title>Falcon Print</title>
+                <link rel="canonical" href="http://mysite.com/example" />
+    </Helmet>
     <BrowserRouter>
     <Rout />
     </BrowserRouter>
